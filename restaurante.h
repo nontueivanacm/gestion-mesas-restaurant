6259537d14f7a10ee3f7e4421ee8258794798684
Nontueivanacm <167045034+nontueivanacm@users.noreply.github.com>
2025-10-18 03:47:27 -0300

    Update restaurante.h

--- restaurante.h
+++ restaurante.h
@@ -1,1 +1,24 @@
+// Declaración de la clase Restaurante
+#ifndef RESTAURANTE_H
+#define RESTAURANTE_H
 
+#include <vector>
+#include <queue>
+#include "mesa.h"
+using namespace std;
+
+class Restaurante {
+private:
+    vector<Mesa> mesas;
+    queue<string> colaEspera;
+    string archivoMesas = "mesas.dat";
+
+public:
+    void cargarMesas();
+    void guardarMesas();
+    void mostrarMesas();
+    void recibirCliente(string nombre);
+    void liberarMesa(int numero, int ganancia);
+};
+
+#endif
