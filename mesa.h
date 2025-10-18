9a2f6ac7b98ec05b3757fb3532f2ededf12c848c
Nontueivanacm <167045034+nontueivanacm@users.noreply.github.com>
2025-10-18 03:41:27 -0300

    Update mesa.h

--- mesa.h
+++ mesa.h
@@ -1,1 +1,27 @@
 // Declaración de la clase Mesa
+#ifndef MESA_H
+#define MESA_H
+
+#include <iostream>
+using namespace std;
+
+class Mesa {
+private:
+    int numero;
+    bool libre;
+    int ganancia;
+
+public:
+    Mesa(int n = 0, bool l = true, int g = 0);
+    void mostrar();
+    void ocupar();
+    void liberar(int gananciaNueva);
+    int getNumero() const;
+    bool estaLibre() const;
+    int getGanancia() const;
+    void setNumero(int n);
+    void setLibre(bool l);
+    void setGanancia(int g);
+};
+
+#endif
